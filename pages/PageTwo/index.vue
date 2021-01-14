936e6fda999ddd5ec3a34375cada6fe46e95787a
i330z <izzazahmed134@gmail.com>
2021-01-14 13:51:36 +0530

    first commit

---
+++ pages/PageTwo/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <h1>Page Two</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi deleniti eveniet consequuntur veniam facilis, perspiciatis fuga vel, facere, explicabo molestiae ipsam quaerat. Explicabo, in? Harum delectus a quibusdam laboriosam repudiandae voluptatibus saepe voluptates eos dolore totam tenetur nobis laudantium eaque at quam, repellendus facilis voluptatum deleniti minima accusamus possimus in! Adipisci odio debitis saepe ea velit culpa reprehenderit soluta nemo.</p>
+   <v-btn to="/">Home</v-btn>
+
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+   <v-col>
+     <h1>Home</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore dolor nostrum, cupiditate iste quis est natus earum. Dolor, nesciunt! Consectetur nihil assumenda quis eum autem, dolore eaque, suscipit impedit ipsam ducimus consequatur molestias tenetur tempora accusamus nesciunt distinctio praesentium animi architecto fugiat dicta modi voluptatem mollitia quae! Fuga vitae facere ipsam eligendi ratione nisi repellat quo qui incidunt, dolore earum unde quas esse rerum aliquam corporis aspernatur saepe repellendus, explicabo quod, minus minima sunt tempore! Voluptate aut nesciunt aperiam voluptatum.</p>
+   <v-btn to="/pagetwo">Page Two</v-btn>
+   </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
