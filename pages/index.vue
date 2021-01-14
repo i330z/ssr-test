8be5fcd4816a6732b1c43d7324db87e2d747b674
i330z <izzazahmed134@gmail.com>
2021-01-14 14:07:26 +0530

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,4 @@
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
-</script>
